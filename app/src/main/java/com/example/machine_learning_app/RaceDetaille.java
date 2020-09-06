5ad475ff5b64e3db4f84160a67359b8189270096
Tareq <tarikelhariri98@gmail.com>
2020-09-06 16:52:39 +0100

    Add Race Detaille Interface

--- app/src/main/java/com/example/machine_learning_app/RaceDetaille.java
+++ app/src/main/java/com/example/machine_learning_app/RaceDetaille.java
@@ -1,16 +1,14 @@
 package com.example.machine_learning_app;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+
+public class RaceDetaille extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_race_detaille);
     }
-
 }
-;
