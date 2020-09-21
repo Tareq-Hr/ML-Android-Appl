61d4e07757e99fd47e13c927118ceb85a7894c12
Tareq <tarikelhariri98@gmail.com>
2020-09-21 17:25:03 +0100

    Add MachineLearning Part (Updating files)

--- app/src/main/java/com/example/machine_learning_app/RaceDetaille.java
+++ app/src/main/java/com/example/machine_learning_app/RaceDetaille.java
@@ -46,7 +46,7 @@ public class RaceDetaille extends AppCompatActivity {
 
         *///2
 
-        IamAuthenticator authenticator = new IamAuthenticator("GEqKoCqVHa5qYuVyFMFyJXQuwOe9tISWcENJtRWQjiVF");
+        IamAuthenticator authenticator = new IamAuthenticator("yWcR9NQnH9roiYJAzjusUd6BFwNWEE_1TQNSltA9_v3h");
 
         VisualRecognition service = new VisualRecognition("2018-03-19", authenticator);
 
@@ -81,7 +81,7 @@ public class RaceDetaille extends AppCompatActivity {
                 .imagesFile(imagesStream)
                 .imagesFilename("Bird")
                 .threshold((float) 0.6)
-                .classifierIds(Arrays.asList("DefaultCustomModel_746826046"))
+                .classifierIds(Arrays.asList("DefaultCustomModel_1641669772"))
                 .build();
         ClassifiedImages result = service.classify(classifyOptions).execute().getResult();
         System.out.println(result.getImages().get(0).getClassifiers().get(0).getClasses().get(0).getXClass());
