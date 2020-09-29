0f4529084f3fb13f956ee86c2808093d839971eb
Tareq <tarikelhariri98@gmail.com>
2020-09-29 22:53:21 +0100

    Update Changes

--- app/src/main/java/com/example/machine_learning_app/RaceDetaille.java
+++ app/src/main/java/com/example/machine_learning_app/RaceDetaille.java
@@ -9,11 +9,18 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
 import com.ibm.cloud.sdk.core.security.IamAuthenticator;
 import com.ibm.watson.visual_recognition.v3.VisualRecognition;
 import com.ibm.watson.visual_recognition.v3.model.ClassifiedImages;
@@ -26,6 +33,7 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class RaceDetaille extends AppCompatActivity {
 
@@ -146,10 +154,30 @@ public class RaceDetaille extends AppCompatActivity {
             }
         });
         thread.start();
-    }
+        /*/ Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                                            // This method is called once with the initial value and again
+                                            // whenever data at this location is updated.
+                GenericTypeIndicator value = dataSnapshot.getValue(GenericTypeIndicator.class);
+                                            Log.d("Firebase", "Value is: " + value);
+
+                                        }
+
+                                        @Override
+                                        public void onCancelled(DatabaseError error) {
+                                            // Failed to read value
+                                            Log.w("Firebase", "Failed to read value.", error.toException());
+                                        }
     /*@Override
     public void onBackPressed(){
         progressDialog.dismiss();
 
-    }*/
+
+});}*/
+    }
 }
